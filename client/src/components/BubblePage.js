7668cad0c4fd8c03b3c05b89bf6c3c29682ec527
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-01-23 12:10:04 -0500

    added useEffect and axiosWithAuth to BubblePage to request colorList on component mount

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  
+  useEffect(() => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+    axiosWithAuth().get('/colors')
+      .then(res => {
+        const colorsArr = res.data;
+        setColorList(colorsArr);
+      })
+      .catch(err => console.log(err))
+  }, [])
+ 
 
   return (
     <>
